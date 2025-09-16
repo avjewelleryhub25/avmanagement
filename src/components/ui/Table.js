1ee805bb34f00b42069916b0e98a3467fa1b304b
Abhishek Rajput <168184952+abhiayasya@users.noreply.github.com>
2025-09-16 10:57:02 +0530

    feat: Refactor Table component to improve data access and readability

--- src/components/ui/Table.js
+++ src/components/ui/Table.js
@@ -2,6 +2,14 @@
 import React from "react";
 
 export default function Table({ columns, data, onRowClick }) {
+
+  function getValueByPath(obj, path) {
+    if (!path) return undefined;
+    return path
+      .replace(/\?/g, "") // remove all "?" so productId?.name → productId.name
+      .split(".")
+      .reduce((acc, key) => acc?.[key], obj);
+  }
   return (
     <div className="overflow-x-auto">
       <table className="w-full border-collapse border border-gray-300">
@@ -19,18 +27,18 @@ export default function Table({ columns, data, onRowClick }) {
             <tr
               key={rowIndex}
               onClick={() => onRowClick && onRowClick(row)}
-              className={`border-b border-gray-200 hover:bg-amber-50 cursor-pointer ${
-                row.stock < 10 ? "text-red-500" : ""
-              }`}
+              className={`border-b border-gray-200 hover:bg-amber-50 cursor-pointer ${row.stock < 10 ? "text-red-500" : ""
+                }`}
             >
               {columns.map((col, colIndex) => {
-                console.log(row["productId.name"],"--------------",row);
-                
-                return(
-                <td key={colIndex} className="p-3">
-                  {col.render ? col.render(row) : row[col.key]}
-                </td>
-              )})}
+                console.log(getValueByPath(row, col.key), "------------");
+
+                return (
+                  <td key={colIndex} className="p-3">
+                    {col.render ? col.render(row) : getValueByPath(row, col.key)}
+                  </td>
+                )
+              })}
             </tr>
           ))}
         </tbody>
